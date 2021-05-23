00b89011dd9959bf4a47c42a703a6921f77f791f
Rebecca Ross <b.laurenross@gmail.com>
2021-05-24 00:22:02 +0100

    Created main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArticlesComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
